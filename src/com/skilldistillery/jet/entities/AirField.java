99101b6192c8b2daf6f6c16f5d03cd7047dd497e
Jacob Nguyen <jn@jacobello.com>
2022-04-03 22:05:48 -0600

    txt. done

--- src/com/skilldistillery/jet/entities/AirField.java
+++ src/com/skilldistillery/jet/entities/AirField.java
@@ -1,8 +1,31 @@
 package com.skilldistillery.jet.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AirField {
 	private List<Jet> jets;
 	
+	public AirField() {
+		jets = new ArrayList<>();
+	}
+
+	public List<Jet> getJets() {
+		return jets;
+	}
+
+	public void setJets(List<Jet> jets) {
+		this.jets = jets;
+	}
+
+	public void setJet(Jet jet) {
+		jets.add(jet);
+	}
+
+	@Override
+	public String toString() {
+		return "AirField jets: " + jets;
+	}
+
 }
+
